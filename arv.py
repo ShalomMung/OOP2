bfd58b952bc910b177c8ce59ba7e60e3a85f959a
ShalomMung <shallomung@outlook.com>
2023-04-25 14:48:56 +0200

    modified again

--- arv.py
+++ arv.py
@@ -1,3 +1,5 @@
+import numpy as np
+    
 class Line:
     def __init__(self, c0, c1):
         self.c0 = c0
@@ -5,6 +7,13 @@ class Line:
 
     def __call__(self, x):
         return self.c0 + self.c1 * x
+
+    def print_point(self, L, R, n):
+        print("Line")
+        print("x\tf(x)")
+        for x in np.linspace(L, R, n):
+            print(f"{x:.1f}\t{self(x):.1f}")
+
 
 class Parabola:
     def __init__(self, c0, c1, c2):
@@ -15,10 +24,19 @@ class Parabola:
     def __call__(self, x):
         return self.c0 + self.c1 * x + self.c2 * x**2
 
+    def print_point(self, L, R, n):
+        print("Parabel")
+        print("x\tf(x)")
+        for x in np.linspace(L, R, n):
+            print(f"{x:.1f}\t{self(x):.1f}")
+
 if __name__ == "__main__":
     l1 = Line(1, 2)
     p1 = Parabola(1, 2, 3)
 
-    x = 2
-    print(f"Line 1 evaluate at {x}: {l1(x)}")
-    print(f"Parabola 1 evaluate at {x}: {p1(x)}")
+    #x = 2
+    #print(f"Line 1 evaluate at {x}: {l1(x)}")
+    #print(f"Parabola 1 evaluate at {x}: {p1(x)}")
+
+    l1.print_point(0, 10, 4)
+    p1.print_point(0, 10, 4)
