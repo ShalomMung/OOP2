dc20be6c33051bf1815cfa2a76646b8d8f0b6b88
ShalomMung <shallomung@outlook.com>
2023-04-25 14:08:41 +0200

    A modified version of arv.py

--- arv.py
+++ arv.py
@@ -1,5 +1,15 @@
 class Line:
-    pass
+    def __init__(self, c0, c1):
+        self.c0 = c0
+        self.c1 = c1
 
 class Parabola:
-    pass
+    def __init__(self, c0, c1, c2):
+        self.c0 = c0
+        self.c1 = c1
+        self.c2 = c2
+
+if __name__ == "__main__":
+    l1 = Line(1, 2)
+    p1 = Parabola(1, 2, 3)
+    
